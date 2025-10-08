5f1aefe23f4be891234faede2e194243c9eea896
MuratorkunN <orkun.katkat@ozu.edu.tr>
2025-10-08 13:57:50 +0300

    loan prediction initial notebook, qgis shapefile

---
+++ SQL/joins.sql
@@ -0,0 +1,17 @@
+    -- All joins in one query, one by one
+
+    -- INNER JOIN
+
+    -- CROSS JOIN
+
+    -- LEFT JOIN
+
+    -- RIGHT JOIN
+
+    -- FULL JOIN
+
+    -- SELF JOIN
+
+    -- NATURAL JOIN
+
+    
