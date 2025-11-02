adb5c7261aeae4a641815ccd2255de82e138a940
MuratorkunN <orkun.katkat@ozu.edu.tr>
2025-11-02 14:30:43 +0300

    min temperature analysis via sql

--- Frost Prediction/frost.sql
+++ Frost Prediction/frost.sql
@@ -7,3 +7,13 @@ CREATE TABLE frost_data (
 \COPY frost_data (date, min_temperature, frost)
 FROM 'temp_and_frost_data.csv'
 WITH (FORMAT csv, HEADER true);
+
+
+CREATE TABLE frost_data_all (
+    date DATE NOT NULL,
+    min_temperature FLOAT
+);
+
+\COPY frost_data_all (date, min_temperature)
+FROM 'temperature.csv'
+WITH (FORMAT csv, HEADER true);
--- Frost Prediction/frost_tables.sql
+++ Frost Prediction/frost_tables.sql
@@ -1,5 +1,47 @@
-select * from frost_data fd 
-where fd.frost <> 0
+SELECT * FROM  frost_data fd 
+WHERE fd.frost <> 0
 
-select * from frost_data fd 
-where fd.min_temperature <=0
+SELECT * FROM frost_data fd 
+WHERE fd.min_temperature <=0
+
+
+
+
+SELECT * FROM frost_data_all fd 
+
+SELECT
+    TRIM(to_char(date, 'Month')) as month,
+    AVG(min_temp) as average_min_temp
+FROM
+    frost_data_all
+WHERE
+    date >= '1991-01-01' AND date <= '2020-12-31'
+GROUP BY
+    EXTRACT(MONTH FROM date), month
+ORDER BY
+    EXTRACT(MONTH FROM date);
+    
+   
+   
+   
+WITH monthly_mins AS (
+    SELECT
+        EXTRACT(YEAR FROM date) as year,
+        EXTRACT(MONTH FROM date) as month,
+        MIN(min_temp) as min_monthly_temp
+    FROM
+        frost_data_all
+    WHERE
+        date >= '1991-01-01' AND date <= '2020-12-31'
+    GROUP BY
+        year, month
+)
+SELECT
+    to_char(make_date(2000, month::int, 1), 'Month') as month_name,
+    AVG(min_monthly_temp) as average_of_yearly_mins
+FROM
+    monthly_mins
+GROUP BY
+    month
+ORDER BY
+    month;
